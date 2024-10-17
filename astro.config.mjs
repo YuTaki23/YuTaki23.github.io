4176e6ab427d32616a5fe8fdaf536102ed74b267
YuTaki23 <1826821764@qq.com>
2024-10-17 20:25:45 +0800

    try

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
+    site: 'https://yutaki23.github.io',
     base: '/',
 });
